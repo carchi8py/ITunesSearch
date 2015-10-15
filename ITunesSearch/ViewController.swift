ccbfe96cee2c33d068e7e186a13d2b93c45b47cb
Chris Archibald <carchi8py@gmail.com>
2015-10-14 20:15:58 -0700

    First part of network code complete

--- ITunesSearch/ViewController.swift
+++ ITunesSearch/ViewController.swift
@@ -13,6 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var searchTerm = "Paul Solt"
+        if let escapedSearchTerm = searchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+            var searchString = "https://itunes.apple.com/search?term=\(escapedSearchTerm)&entity=software"
+            print(searchString)
+            
+            if let url = NSURL(string: searchString) {
+                let task  = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler: {
+                    data, response, error in
+                    
+                    if error != nil {
+                        print("Error \(error?.localizedDescription)")
+                    } else {
+                        print("data: \(data)")
+                    }
+                })
+                task.resume()
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
