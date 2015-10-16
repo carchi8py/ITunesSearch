16e9844ee60573f20ab9a00d28e4d1ffa77646db
Chris Archibald <carchi8py@gmail.com>
2015-10-15 21:14:34 -0700

    Added Table view.

--- ITunesSearch/ViewController.swift
+++ ITunesSearch/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var appDataArray = [AppData]()
 
@@ -77,6 +77,21 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Tableview DataSourceDelegate Methods
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell()
+        cell.textLabel?.text = NSDate().description
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 5
     }
 
 
